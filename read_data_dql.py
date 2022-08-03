34f0ddf992f5d2e98a8b15e1b49d35da712750a1
MuhammadQ4 <ikram.qau88@gmail.com>
2022-08-03 15:20:57 -0500

    initial commit

---
+++ read_data_dql.py
@@ -0,0 +1,39 @@
+import yaml
+import mysql.connector
+import pandas as pd
+
+# db = yaml.safe_load(open('config.yaml'))
+db = yaml.safe_load(open('config_mrtssales.yaml'))
+
+config = {
+    'user' : db['user'],
+    'host' : db['host'],
+    'database': db['database'],
+    'password' : db['password'], 
+    'auth_plugin' : 'mysql_native_password'
+}
+
+def execute_sql_logic(_query):
+    """
+        This functions implements the DQL logic i.e., it runs SQL statements. 
+        It takes one input as an argument:
+            _query: a string of an SQL query
+    """
+
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    # cursor = connection.cursor( buffered=True , dictionary=True)
+    # _query = f'select * from actor limit 5;'
+    cursor.execute(_query)
+
+    columns = [col[0] for col in cursor.description]
+    frame = [dict(zip(columns,row)) for row in cursor.fetchall()]
+
+    df = pd.DataFrame(frame)
+    # df.set_index(df.columns[0], inplace=True)
+    cursor.close()
+    connection.close()
+
+    return df
+
+    # print('a record is inserted into the table.')
